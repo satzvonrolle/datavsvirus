a8dc6bb9ed9d4e51efefe1b959b7ffe504c19deb
Manuel Sch <donotwriteemail@example.com>
2020-03-21 13:12:45 +0100

    added dead and recovered cases

--- get_data_jhu.py
+++ get_data_jhu.py
@@ -1,4 +1,7 @@
 import requests
-r = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")  
-with open('data/jhu_data.csv', 'wb') as f:
-    f.write(r.content)
+
+
+for key in ["Confirmed", "Deaths", "Recovered"]:
+    r = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-"+key+".csv")  
+    with open('data/jhu_data_'+key+'.csv', 'wb') as f:
+        f.write(r.content)
