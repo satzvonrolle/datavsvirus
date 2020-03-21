d140d6672c1a40c97002d183f051e07772b45cc3
Manuel Sch <62465372+manumpy@users.noreply.github.com>
2020-03-21 21:32:47 +0100

    adapted refactored script

--- datavsvirus/convert/convert_germany.py
+++ datavsvirus/convert/convert_germany.py
@@ -10,18 +10,7 @@ ENDDATE = datetime(2020, 3, 20)
 # Load the RKI dataset and sort by Landkreis (province), Meldedatum (date) in order to generate time resolved data
 #
 
-data_rki = pd.read_csv("../data/rki_daten.csv").sort_values(by=['Landkreis', 'Meldedatum'])
-
-#
-# Retrieve possible ranges of dates. JHU data starts at January 22
-#
-
-dateset = []
-for row in data_rki.itertuples():
-    dateset.append(datetime.utcfromtimestamp(int(row.Meldedatum/1000)).strftime('%Y%m%d'))
-
-print("All dates",set(dateset), "Min Date", min(dateset), "Max Date", max(dateset))
-
+data_rki = pd.read_csv("../../data/raw/germany/germany.csv").sort_values(by=['Landkreis', 'Meldedatum'])
 
 #
 # Iterate through all rows and reformat rows
@@ -35,15 +24,18 @@ total_cases = 0
 def add_province(all_data, province_data):
 
     prevdata = 0
+    
     for year in range(2020, 2040): # Let's hope there is no Corona anymore in 2040
         if year<=now.year:
             for month in range(1,13):
                 for day in range(1, monthrange(year, month)[1]+1):
-                    print(year,month,day)
+
                     thedate = datetime(year, month, day)
                     
                     if thedate>STARTDATE and thedate<ENDDATE:
+                        
                         dkey = str(month)+"/"+str(day)+"/"+str(year)[:-2]
+                        
                         if dkey not in province_data:
                             province_data[dkey] = prevdata
                         else:
@@ -83,31 +75,8 @@ for row in data_rki.itertuples():
 add_province(all_data, province_data) # add last province, which is not covered in loop
 
 df_new = pd.DataFrame([x for x in all_data])
+df_new.to_csv('../../data/converted/germany.csv', index=False)
 
-df_new.to_csv("../data/data_fused_rki.csv") 
-
-
-jhu_df = pd.read_csv('../data/jhu_data_Confirmed.csv')
-
-# Remove germany from existing JHU data
-df_new = df_new.drop(df_new.loc[(df_new['Country/Region'] == 'Germany') & (df_new['Province/State'].isnull())].index)
-#df_new = pd.concat([jhu_df, df_new], ignore_index=True)
-
-
-
-df_out = pd.concat([df_new, jhu_df], ignore_index=True)           
-
-def get_key(key_data): # Make date in form 1/23/2020 sortable
-    splitdata = key_data.split("/")
-    return splitdata[2]+splitdata[1].zfill(2)+splitdata[0].zfill(2)
-    
-# Sorts columns containing 2020, 2021, 2022 etc.
-#df_out = df_out.reindex_axis(sorted(df_out.columns, key=lambda x: get_key(x) if "202" in x else x), axis=1)
-
-
-
-
-df_out.to_csv('../data/jhu_data_Confirmed_with_germany.csv', index=False)
 
 
 
