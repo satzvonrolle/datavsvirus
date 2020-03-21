ef3d8cbf23a1b7211b852cb43e4db703bd9a27e3
Manuel Sch <62465372+manumpy@users.noreply.github.com>
2020-03-21 22:36:57 +0100

    refactored unit test, removed trailing 0

--- datavsvirus/unittest/test.py
+++ datavsvirus/unittest/test.py
@@ -18,33 +18,34 @@ def extract_cases(name):
     df = pd.read_csv(BASEDIR + name)
     return df['totale_casi'].to_numpy()
 """
-🍝 Code 
+🍝 Code End
 """
 
 
 class Test(unittest.TestCase):
 
-    def test_rki_germany_samples(self):
+    def test_data_raw(self):
     
         #
         # Check with some samples if number of cases is correct for germany and italy.
         #
         
-        data_germany = pd.read_csv("../../data/raw/germany/germany.csv")
-        self.assertGreater(data_germany["AnzahlFall"].sum() ,  16000)
-        
-
         names = [name for name in os.listdir(BASEDIR) if name.endswith('.csv')]
         df_italy = pd.DataFrame( {convert_to_datestring(name): extract_cases(name) for name in names})
         self.assertEqual(df_italy["2/25/20"].sum() ,  322)
         
+        data_germany = pd.read_csv("../../data/raw/germany/germany.csv")
+        self.assertGreater(data_germany["AnzahlFall"].sum() ,  16000)
+        
+    def test_rki_germany_samples(self):
+
         #
         # Load current and deprecated data for germany. Check if numbers match.
         #
         
         data_germany = pd.read_csv("../../data/converted/germany.csv")
         data_germany_depreacted = pd.read_csv("../../data/converted/germany_deprecated.csv")
-        self.assertEqual(data_germany["03/18/20"].sum(), data_germany_depreacted["3/18/20"].sum())
+        self.assertEqual(data_germany["3/18/20"].sum(), data_germany_depreacted["3/18/20"].sum())
 
 if __name__ == '__main__':
     unittest.main()
