46df1f62f50b506b838c188adb66d01dfc39300f
Manuel Sch <62465372+manumpy@users.noreply.github.com>
2020-03-21 22:30:34 +0100

    completed unit tests

--- datavsvirus/unittest/test.py
+++ datavsvirus/unittest/test.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 
 
+BASEDIR = '../../data/raw/italy/'
+        
 """
 🍝 Code (duplicate in convert_italy.py)
 """
@@ -13,7 +15,7 @@ def convert_to_datestring(name):
     return '%i/%i/20' % (month, day)
 
 def extract_cases(name):
-    df = pd.read_csv(basedir + name)
+    df = pd.read_csv(BASEDIR + name)
     return df['totale_casi'].to_numpy()
 """
 🍝 Code 
@@ -31,12 +33,10 @@ class Test(unittest.TestCase):
         data_germany = pd.read_csv("../../data/raw/germany/germany.csv")
         self.assertGreater(data_germany["AnzahlFall"].sum() ,  16000)
         
-        data_italy = pd.read_csv("../../data/raw/germany/italy.csv")
-        basedir = '../../data/raw/italy/'
-        names = [name for name in os.listdir(basedir) if name.endswith('.csv')]
+
+        names = [name for name in os.listdir(BASEDIR) if name.endswith('.csv')]
         df_italy = pd.DataFrame( {convert_to_datestring(name): extract_cases(name) for name in names})
-        df_italy["2/25/20"].sum()
-        self.assertEqual(data_rki["AnzahlFall"].sum() ,  322)
+        self.assertEqual(df_italy["2/25/20"].sum() ,  322)
         
         #
         # Load current and deprecated data for germany. Check if numbers match.
@@ -44,7 +44,7 @@ class Test(unittest.TestCase):
         
         data_germany = pd.read_csv("../../data/converted/germany.csv")
         data_germany_depreacted = pd.read_csv("../../data/converted/germany_deprecated.csv")
-        self.assertEqual(data_germany["03/18/20"].sum() == data_germany_depreacted["03/18/20"].sum())
+        self.assertEqual(data_germany["03/18/20"].sum(), data_germany_depreacted["3/18/20"].sum())
 
 if __name__ == '__main__':
     unittest.main()
