f23f3693d2c22bfcfcc5b753b06cd7f6927397bd
Lorenz Hehn <lorenz.hehn@outlook.com>
2020-03-21 14:55:21 +0100

    [ADD] load dataset from italy

---
+++ data/get_data_ita.py
@@ -0,0 +1,9 @@
+import requests
+
+feb = ['02%02i' % i for i in range(24, 30)]
+mar = ['03%02i' % i for i in range(1, 21)]
+
+for key in feb + mar:
+    r = requests.get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-2020" + key + ".csv")
+    with open('ita_data_'+key+'.csv', 'wb') as f:
+        f.write(r.content)
