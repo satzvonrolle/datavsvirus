e44ed4f06e5e3339d0a5bace12b449bd54e6e1e4
Lorenz Hehn <lorenz.hehn@outlook.com>
2020-03-22 09:19:33 +0100

    removed rows in italy dataset; fix #2

--- datavsvirus/convert/convert_italy.py
+++ datavsvirus/convert/convert_italy.py
@@ -4,6 +4,8 @@ import os
 basedir = '../../data/raw/italy/'
 
 names = [name for name in os.listdir(basedir) if name.endswith('.csv')]
+tmp = pd.read_csv(basedir + names[0])
+
 
 def convert_to_datestring(name):
     date = name[6:10]
@@ -11,11 +13,11 @@ def convert_to_datestring(name):
     day = int(date[2:])
     return '%i/%i/20' % (month, day)
 
+
 def extract_cases(name):
     df = pd.read_csv(basedir + name)
     return df['totale_casi'].to_numpy()
 
-tmp = pd.read_csv(basedir + names[0])
 
 dd = {convert_to_datestring(name): extract_cases(name) for name in names}
 dd['Province/State'] = tmp['denominazione_provincia']
@@ -24,6 +26,7 @@ dd['Lat'] = tmp['lat']
 dd['Long'] = tmp['long']
 
 df = pd.DataFrame(dd)
+df = df.drop(df.loc[df['Province/State'] == 'In fase di definizione/aggiornamento'].index)  # issue 2
 
 for column in pd.read_csv('../../data/raw/reference.csv').columns:
     if column not in df:
--- test/test.py
+++ test/test.py
@@ -4,22 +4,12 @@ import pandas as pd
 reference = pd.read_csv('../data/raw/reference.csv')
 fused = pd.read_csv('../data/fused.csv')
 
-for country in reference['Country/Region']:  # country = 'Italy'
+for country in reference['Country/Region']:
     ref = reference.loc[reference['Country/Region'] == country]
     fus = fused.loc[fused['Country/Region'] == country]
-    if not np.array_equal(ref.to_numpy()[:, 4:].astype(float), fus.to_numpy()[:, 4:].astype(float)):
+    if not np.array_equal(ref.iloc[:, 4:].to_numpy(), fus.iloc[:, 4:].to_numpy()):
         print(f'Comparing {country}')
         ref_series = ref.sum()[4:]
         fus_series = fus.sum()[4:]
         diff_series = fus_series - ref_series
         print(diff_series)
-
-# # loop
-
-# jhu_germany = jhu.loc[(jhu['Country/Region'] == 'Italy')].sum()[4:]
-# jhu_italy = jhu.loc[(jhu['Country/Region'] == 'Italy')].sum()[4:]
-
-# df = pd.read_csv('../data/jhu_data_Confirmed_with_Italy.csv')
-# df_italy = df.loc[(df['Country/Region'] == 'Italy')].sum()[4:]
-
-# italy = jhu_italy - df_italy
