6ac0303ecc53003b2bc3571fdd40fe19f3c8151d
Wolfgang Noichl <w.noichl@gmail.com>
2020-03-22 18:04:31 +0100

    readme, added youtube

--- datavsvirus/run.py
+++ datavsvirus/run.py
@@ -9,10 +9,10 @@ ipython = get_ipython()
 # and bash may not be available on Windows. Thus, we run by ipython magic.
 # (which isn't great either, because it doesn't use fresh python instances, but well...)
 
-# os.chdir(os.path.join(script_dir, 'load'))
-# for country in os.listdir('./'):
-#     print(f"Loading, running {country}")
-#     ipython.magic(f'run {country}')
+os.chdir(os.path.join(script_dir, 'load'))
+for country in os.listdir('./'):
+    print(f"Loading, running {country}")
+    ipython.magic(f'run {country}')
 
 os.chdir(os.path.join(script_dir, 'convert'))
 for country in os.listdir('./'):
